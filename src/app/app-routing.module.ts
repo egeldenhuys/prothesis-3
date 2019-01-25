ca7595be23cbcdbb827a10eb93bd66239b0f0beb
CDuPlooy <armandconnor.duplooy@gmail.com>
2019-01-25 21:42:04 +0200

    WIP

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { DreamsComponent } from './pages/dreams/dreams.component';
+
+const routes: Routes = [
+  {
+    path: 'dreams',
+    component: DreamsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,10 +10,13 @@ import { environment } from '../environments/environment';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 
+import { DreamsComponent } from './pages/dreams/dreams.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DreamsComponent
   ],
   imports: [
     NgbModule,
