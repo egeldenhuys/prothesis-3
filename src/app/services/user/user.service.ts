8286f6285302f1a5a0d1c20c871c415d0641f26c
CDuPlooy <armandconnor.duplooy@gmail.com>
2019-01-26 11:01:46 +0200

    Removed extra braces

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -14,9 +14,9 @@ export class UserService {
   test() {
     for (const dream of this.qs.dreams) {
       const obj = toAnswer(dream);
-      this.db.collection('users').doc('QGhR84dfAC7hZ35gX2jj').collection('answers').doc(dream.id).set({
+      this.db.collection('users').doc('QGhR84dfAC7hZ35gX2jj').collection('answers').doc(dream.id).set(
         obj
-      }).then(); // Check and handle errors
+      ).then(); // Check and handle errors
     }
   }
 }
